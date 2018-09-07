45ad1c45cedfe622dbd8c3862056eec4c6ecbf28
xiaobeic <756048962@qq.com>
2018-09-07 18:24:39 +0800

    Init project

---
+++ src/main/java/com/info/shane/ShaneApplication.java
@@ -0,0 +1,12 @@
+package com.info.shane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShaneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShaneApplication.class, args);
+    }
+}
---
+++ src/test/java/com/info/shane/ShaneApplicationTests.java
@@ -0,0 +1,16 @@
+package com.info.shane;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShaneApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
